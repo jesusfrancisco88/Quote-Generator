432233b6a0b6f57f8a6481bffab9b07412499173
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-20 17:19:51 -0600

    Renderizado

--- src/random-quote/random-quote.js
+++ src/random-quote/random-quote.js
@@ -7,6 +7,7 @@ const fetchQuote = async () => {
     const url = `https://quote-garden.onrender.com/api/v3/quotes?time=${timeStamp}`;
     const res = await fetch(url);
     const data = await res.json();
+    // console.log(data.data[Math.floor(Math.random() * data.data.length)]);
     return data.data[Math.floor(Math.random() * data.data.length)];    
   } catch (error) {
     throw new Error(`Error Message: ${error.message}`)
@@ -14,16 +15,44 @@ const fetchQuote = async () => {
 }
 
 
-
-
-
 export const quoteGen = async ( element ) => {
+  element.innerHTML = `<span class='loading-message'>Loading...</span>`;
+
+  //* HTML/CSS //*
+  const button = document.createElement('button');
+  button.innerText = 'random'
+  button.classList.add('button');
+
+  const quote = document.createElement('blockquote');
+  quote.classList.add('quote')
+
+  const author = document.createElement('h2');
+  author.classList.add('author');
+
+  const category = document.createElement('p');
+  category.classList.add('category')
 
 
 
+  //* Render //*
+  const renderQuote = ( data ) => {
+    quote.innerHTML = `"${data.quoteText}"`;
+    author.innerHTML = `${data.quoteAuthor}`;
+    category.innerHTML = `${data.quoteGenre}`;
 
+    element.replaceChildren(quote,author,category, button);
+  }
   
+  //* Button
+  button.addEventListener('click', function() {
+    element.innerHTML = `<span class='loading-message'>Loading...</span>`;
+
+    fetchQuote()
+    .then( data => renderQuote(data) )
+    .catch( error => console.error(error.message) );
+  })
+
   fetchQuote()
-  .then(data => console.log(data.quoteText))
-  .catch(error => console.error(error.message));
+  .then( data => renderQuote(data) )
+  .catch( error => console.error(error.message) );
 }
